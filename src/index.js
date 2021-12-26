1ef7689ec8deb7494313e94d0bf742fc834f9d4d
Robert Edmonds <bobby.edmonds89@gmail.com>
2021-12-26 16:31:43 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -7,8 +7,33 @@ function upload(pic){
 function dogName(name){
     let list = document.createElement('li')
     list.textContent = name
-    document.querySelector('#dog-breeds').append(list)
+    list.addEventListener('click',changeColor)
+    // console.log(document.querySelector('#breed-dropdown').value)
+    list.addEventListener('onclick', dogMatch)
+    document.querySelector('#dog-breeds').append(list)    
 }
+function changeColor(spot){
+        if(spot.path[0].style.color == ""){
+            console.log('color')
+            spot.path[0].style.color = "red"
+        }else{
+            spot.path[0].style.color = ""
+        }
+    }
+function dogMatch(array){
+    let list = document.createElement('li')
+    let target = document.querySelector('#breed-dropdown').value
+    // array.filter(char =>char.slice(0, target.length) === target)
+    target.addEventListener('submit', function(){
+        if (array.slice(0, target.length) === target){
+       return list.textContent = array}
+    })
+    
+    console.log('click')
+    console.log(list)
+    document.querySelector('#dog-breeds').append(list)    
+}
+
 
 function renderPage(){
     const imgUrl = fetch("https://dog.ceo/api/breeds/image/random/4")
@@ -19,17 +44,15 @@ function renderPage(){
     const breedUrl = fetch('https://dog.ceo/api/breeds/list/all')
     .then(res => res.json())
     .then(json => {
-        console.log(json)
-        const dog = Object.values(json)[0]
-        dog.message.forEach(dogName)
-        // if(dog.values > [0]){
-        //     return Array.from(dog)
-        // }
-        // dog.forEach(dogName)
-    })
-    console.log(breedUrl)
-        
+        const obDog = Object.keys(json.message)
+        // const dog = Object.values(json)
+        // const dogList = Array.from(dog)
+        obDog.filter(dogMatch)
+        obDog.forEach(dogName)
+        // obDog.filter(dogMatch)
+    }) 
     return (breedUrl, imgUrl)
+
 
 }
 renderPage()
